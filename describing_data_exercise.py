4674f2f9de3e5054065ce9d3e9c7c24484120382
swiderskis <66870324+swiderskis@users.noreply.github.com>
2020-07-06 08:58:15 +0100

    Add files via upload

---
+++ describing_data_exercise.py
@@ -0,0 +1,27 @@
+"""
+The first function reads the file "auto-mpg.data", defines its headers, 
+accounts for whitespace, and returns it.
+The second function finds the mean of the agg_var variable for each group_var
+group, and returns it.
+The data file used can be obtained from
+https://archive.ics.uci.edu/ml/datasets/Auto+MPG.
+This is an exercise for the "Python Data Analysis and Visualisation" course on
+Educative.
+"""
+
+import pandas as pd
+def read_csv():
+    
+     #Headers of columns in file "auto-mpg.data"
+    names = ["mpg", "cylinders", "displacement", "horsepower", "weight", 
+             "acceleration", "model_year", "origin", "car_name"]
+    df = pd.read_csv("auto-mpg.data", header = None, names = names, 
+                     delim_whitespace = True)
+    
+    return df
+
+def group_aggregation(df, group_var, agg_var):
+    
+    df_mean = df.groupby(group_var)[agg_var].mean()
+    
+    return df_mean
